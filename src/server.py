3acb733df8aa1a144e7f1984d7ed964c65ac4f09
Myles Megyesi <mmstix8@gmail.com>
2010-04-27 15:35:32 -0500

    fixed server docs

--- src/server.py
+++ src/server.py
@@ -3,15 +3,15 @@
 # @author Paul Schorfheide
 
 import socket, time, re, threading, sys
-#@var games: the list of games waiting for players
+#@var games the list of games waiting for players
 games = []
-#@var LISTEN_PORT: the port to listen on
+#@var LISTEN_PORT the port to listen on
 LISTEN_PORT = 5555
-#@var LOGFILE_NAME: the name of the file to log messages to
+#@var LOGFILE_NAME the name of the file to log messages to
 LOGFILE_NAME = 'server.log'
-#@var TIMEOT: the number of seconds to keep games in the queue
+#@var TIMEOT the number of seconds to keep games in the queue
 TIMEOUT = 10
-#@var logfile: the logfile handle
+#@var logfile the logfile handle
 logfile = 0
 
 ## the listener
@@ -29,8 +29,8 @@ def listenForRequests():
             pass
 
 ## handle join requests from client
-#@param client: the client socket
-#@param client_addr: the address of the client socket
+#@param client the client socket
+#@param client_addr the address of the client socket
 def joinGame(client, client_addr):
     global games
     if len(games) == 0:
@@ -46,8 +46,8 @@ def joinGame(client, client_addr):
         logAndSend(client, client_addr, 'JOIN ' + str(addr))
 
 ## increment the player waiting count for a game
-#@param client: the client socket
-#@param client_addr: the address of the client
+#@param client the client socket
+#@param client_addr the address of the client
 def addPlayer(client, client_addr):
     global games
     entry = [(a,b) for (a,b) in games if a == client_addr]
@@ -64,8 +64,8 @@ def addPlayer(client, client_addr):
     logAndSend(client, client_addr, 'REQUEST PENDING')
 
 ## start a timer to cancel a game
-#@param game: the game to wait on
-#@param create: whether or not to create a timer if one does not exist
+#@param game the game to wait on
+#@param create whether or not to create a timer if one does not exist
 def makeTimer(game, create=False):
     global timers
     if game in timers:
@@ -76,9 +76,9 @@ def makeTimer(game, create=False):
         timers[game].start()
 
 ## parse a message from a client
-#@param s: the message
-#@param client: the client socket
-#@param client_addr: the address of the client socket
+#@param s the message
+#@param client the client socket
+#@param client_addr the address of the client socket
 def parseRequest(s, client, client_addr):
     global timers
     arr = s.split('###')
@@ -105,8 +105,8 @@ def parseRequest(s, client, client_addr):
         logAndSend(client, client_addr, 'ERROR: PARSE ERROR')
 
 ## change the leader of a game
-#@param old: the old leader
-#@param new: the new leader
+#@param old the old leader
+#@param new the new leader
 def changeLeader(old, new):
     global games
     entry = [(a,b) for (a,b) in games if a == old]
@@ -115,7 +115,7 @@ def changeLeader(old, new):
         games[i] = (new, entry[0][1])
     
 ## remove a game
-#@param game: the game to remove
+#@param game the game to remove
 def clearGame(game):
     global games
     i = [(a,b) for (a,b) in games if a == game]
@@ -124,22 +124,22 @@ def clearGame(game):
     log('removed game ' + str(game))
 
 ## send a message and log the send event
-#@param client: the socket to send to
-#@param client_addr: the address to send to
-#@param msg: the message to send
+#@param client the socket to send to
+#@param client_addr the address to send to
+#@param msg the message to send
 def logAndSend(client, client_addr, msg):
     log('SENT ' + msg + ' TO '+ str(client_addr))
     client.send(msg)
 
 ## log a message
-#@param s: the message to log
+#@param s the message to log
 def log(s):
     global logfile
     logfile.write('[' + time.asctime() + '] ' + s + '\r\n')
     logfile.flush()
 
 ## parse an address from a message
-#@param s: the string to parse
+#@param s the string to parse
 def parseAddr(s):
     m = re.search('\(\'([\d\.]+)\', (\d+)\)', s)
     return (m.group(1), int(m.group(2)))
